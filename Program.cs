9136b10c3e0b1c69413ba0b91baf4373a4a27708
Enderson Nobre <nobreenderson@gmail.com>
2023-05-23 11:41:16 -0300

    Concluindo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone smt1 = new Nokia("IXT222", "Lumia", "AX9383439843", 6);
+smt1.Ligar();
+smt1.ReceberLigacao();
+smt1.InstalarAplicativo("WhatsApp");
+
+
+Smartphone smt2 = new Iphone("APT488", "C4", "FTV888999", 8);
+smt2.Ligar();
+smt2.ReceberLigacao();
+smt2.InstalarAplicativo("WhatsApp");
